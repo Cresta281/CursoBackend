4cf50a67595dff3068b1f7821fa4f62148fcb860
Cristobal Rodriguez <tobal.rodriguez@gmail.com>
2022-07-11 15:32:18 -0300

    correccion entrega

--- Clases/Clase4.js
+++ Clases/Clase4.js
@@ -1,51 +1,49 @@
 const fs = require("fs")
 
 
-
-
-function Save(producto) {
+class Productos {
+    constructor (nombre, precio, id) {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+    async Save(producto) {
    
-    fs.readFileSync("clase4Arr.json","utf-8",(data))
-    const array = JSON.parse(data)
-    array.bebidas.push(producto)
-    fs.writeFileSync('clase4Arr.json',JSON.stringify(array, null, 2))
+        await fs.readFileSync("clase4Arr.json","utf-8",(data))
+        const array = JSON.parse(data)
+        array.bebidas.push(producto)
+        fs.writeFileSync('clase4Arr.json',JSON.stringify(array, null, 2))
+        
+    }
+    async getById(number) {
+        await fs.readFileSync("clase4Arr.json","utf-8",(data))
+        const information = JSON.parse(data)
+        const itemId = information.find(information.id == number)
+        return itemId
+    }
+    async getAll() {
+        fs.readFileSync("clase4Arr.json","utf-8",(data))
+        const information = JSON.parse(data)
+        return information 
+    }
+    async deleteById(number) {
+        await fs.readFileSync("clase4Arr.json","utf-8",(data))
+        const information = JSON.parse(data)
+        const findId = information.find(information.id == number)
+        const deleteId = information.splice(...findId)
+        return deleteId
+    }
+    async deleteAll() {
+        await fs.readFileSync("clase4Arr.json","utf-8",(data))
+        let information = JSON.parse(data)
+        information = []
+        fs.writeFileSync('clase4Arr.json',JSON.stringify(information, null, 2))
+    }
     
 }
-
-Save({nombre:"Fernet",precio:1200,id: 1})
-
-function getById(number) {
-    fs.readFileSync("clase4Arr.json","utf-8",(data))
-    const information = JSON.parse(data)
-    const itemId = information.find(information.id == number)
-    return itemId
-}
-
+const Fernet = new Productos('Fernet',1200,1)
+Save(Fernet)
 getById(1)
-
-function getAll() {
-    fs.readFileSync("clase4Arr.json","utf-8",(data))
-    const information = JSON.parse(data)
-    return information 
-}
-
 getAll()
-
-function deleteById(number) {
-    fs.readFileSync("clase4Arr.json","utf-8",(data))
-    const information = JSON.parse(data)
-    const findId = information.find(information.id == number)
-    const deleteId = information.splice(...findId)
-    return deleteId
-}
-
 deleteById(1)
-
-function deleteAll() {
-    fs.readFileSync("clase4Arr.json","utf-8",(data))
-    let information = JSON.parse(data)
-    information = []
-    fs.writeFileSync('clase4Arr.json',JSON.stringify(information, null, 2))
-}
-
 deleteAll()
