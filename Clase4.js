3c93d6e9fd8f642271b6fc775b400bba261c1357
Cristobal Rodriguez <tobal.rodriguez@gmail.com>
2022-07-20 11:34:41 -0300

    correcciones con Lu

--- Clase4.js
+++ Clase4.js
@@ -1,107 +1,15 @@
-const fs = require("fs")
+
 const express = require ('express')
-const { prototype } = require("events")
-const { info } = require("console")
 const app = express()
 const PORT = 8080
 
 const server = app.listen (PORT, () => {
-    console.log(`servidor express escuchando en el puerto ${PORT}`)
+    console.log(`servidor express escuchando en el puerto ${server.address().PORT}`)
 })
 
-app.get('/productos', (req, res) => {
-    let productos = new Productos();
-    let arrayProductos = productos.getAll();
 
-    console.log(arrayProductos);
-    res.send(JSON.stringify({'arrayProductos': arrayProductos}));
+const Productos = require('./Container')
+const data = new Productos('productos.json')
+app.get('/products', (req, res) => {
+    data.getAll().then.getRandom().then((a) => res.send(a))
 })
-
-app.get('/productoRandom',(req,res) => {
-    let producto = new Productos();
-    let arrayProductos = producto.getAll();
-    function randomize(arrayProductos) {
-        return Math.floor(Math.random(arrayProductos.length))
-    }
-
-    let infoProducto = arrayProductos[randomize(arrayProductos)];
-    res.send(JSON.stringify({'infoProducto': infoProducto}))
-})
-
-class Productos {
-    constructor (nombre =  '', precio = '', id = '') {
-        this.nombre = nombre
-        this.precio = precio
-        this.id = id
-    }
-    Save() {
-        try {
-            fs.readFile('clase4Arr.json', 'utf-8', (err, data) => {
-                if (err) {
-                    console.log('Se produjo un error al recuperar la info del archivo');
-                }
-
-                let arrayProductos = JSON.parse(data);
-                
-                let infoNuevoProducto = {"nombre": this.nombre, "precio": this.precio, "id": this.id};
-                arrayProductos.productos.push(infoNuevoProducto);
-                console.log(arrayProductos);
-
-                fs.writeFile('clase4Arr.json', JSON.stringify(arrayProductos, null, 2), () => {
-                    console.log("Se guardo correctamente el producto con ID: " + this.id);
-                })
-            });
-            
-            return this.id;
-        }
-        catch(err){
-            return console.log(err)
-        }
-    }
-    GetById(idProducto) {
-        fs.readFile('clase4Arr.json', 'utf-8', (err, data) => {
-            let arrayProductos = JSON.parse(data);
-            let itemId = arrayProductos.productos.filter(x => x.id == idProducto);
-
-            if (itemId.length == 0) {
-                return null;
-            } else {
-                console.log(itemId)
-                // return itemId;
-            }
-        })
-    }
-    getAll() {
-        fs.readFile( 'clase4Arr.json', 'utf-8', (err, data) => {
-            let arrayProductos = JSON.parse(data);
-            //console.log(arrayProductos)
-            return arrayProductos;
-        });
-    }
-    deleteById(idProducto) {
-        fs.readFile('clase4Arr.json', 'utf-8', (err, data) => {
-            let arrayProductos = JSON.parse(data);
-
-            let arrayProductosFiltrados = arrayProductos.productos.filter(x => x.id != idProducto);
-            arrayProductos.productos = arrayProductosFiltrados
-            fs.writeFile('clase4Arr.json', JSON.stringify(arrayProductos), () => {
-                console.log('Se borro correctamente el producto con ID: ' + idProducto)
-            })
-        })
-    }
-    deleteAll() {
-        fs.readFile('clase4Arr.json', 'utf-8', (err, data) => {
-            let arrayVacio = {};
-            arrayVacio.productos = [];
-
-            fs.writeFile('clase4Arr.json', JSON.stringify(arrayVacio), () => {
-                console.log('Se eliminaron todos los items');
-            })
-        })
-    }
-}
-//Fernet.Save();
-//Fernet.GetById(2);
-//Fernet.getAll()
-//Fernet.deleteById(1);
-//Fernet.deleteAll();
---
+++ Container.js
@@ -0,0 +1,96 @@
+const fs = require("fs")
+
+class Productos {
+    constructor (nombre =  '', precio = '', id = '') {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+    Save = async() => {
+        try {
+            await fs.promises.readFile('clase4Arr.json', 'utf-8', (err, data) => {
+                if (err) {
+                    console.log('Se produjo un error al recuperar la info del archivo');
+                }
+
+                let arrayProductos = JSON.parse(data);
+                
+                let infoNuevoProducto = {"nombre": this.nombre, "precio": this.precio, "id": this.id};
+                arrayProductos.productos.push(infoNuevoProducto);
+                console.log(arrayProductos);
+
+               fs.writeFile('clase4Arr.json', JSON.stringify(arrayProductos, null, 2), () => {
+                    console.log("Se guardo correctamente el producto con ID: " + this.id);
+                }) //Aca no puse el await porque en teoria ya esta puesto en la funcion que lo envuelve, nose si es asi (?)
+            });
+            
+            return this.id;
+        }
+        catch(err){
+            return console.log(err)
+        }
+    }
+    getById = async(idProducto) => {
+        await fs.promises.readFile('clase4Arr.json', 'utf-8', (err, data) => {
+            let arrayProductos = JSON.parse(data);
+            let itemId = arrayProductos.productos.filter(x => x.id == idProducto);
+
+            if (itemId.length == 0) {
+                return null;
+            } else {
+                console.log(itemId)
+                return (JSON.stringify(itemId))
+            }
+        })
+    }
+    getAll = async() => {
+        await fs.promises.readFile( 'clase4Arr.json', 'utf-8', (err, data) => {
+            let arrayProductos = JSON.parse(data);
+            //console.log(arrayProductos)
+            return (JSON.stringify(arrayProductos));
+        });
+    }
+    deleteById = async(idProducto) => {
+        await fs.promises.readFile('clase4Arr.json', 'utf-8', (err, data) => {
+            let arrayProductos = JSON.parse(data);
+
+            let arrayProductosFiltrados = arrayProductos.productos.filter(x => x.id != idProducto);
+            arrayProductos.productos = arrayProductosFiltrados
+             fs.writeFile('clase4Arr.json', JSON.stringify(arrayProductos), () => {
+                console.log('Se borro correctamente el producto con ID: ' + idProducto)
+            })
+        })
+    }
+    deleteAll = async() => {
+        await fs.promises.readFile('clase4Arr.json', 'utf-8', (err, data) => {
+            let arrayVacio = {};
+            arrayVacio.productos = [];
+
+            fs.writeFile('clase4Arr.json', JSON.stringify(arrayVacio), () => {
+                console.log('Se eliminaron todos los items');
+            })
+        })
+    }
+    getRandom = async() => {
+        try{
+            await fs.promises.readFile('clase4Arr.json', 'utf-8',(err,data))
+            const productos = JSON.parse(data)
+            const id = await (Math.floor(Math.random() * (productos.length)) + 1)
+            if (id <= productos.length) {
+                return this.getById(id);
+            }
+        }catch(err){
+        console.error({code: 'Error', message: err})                
+        }
+       
+
+    }
+
+}
+module.exports = Productos
+let Fernet = new Productos ('Lucia', 9999, 4)
+//Fernet.Save();
+//Fernet.getById(4);
+//Fernet.getAll()
+//Fernet.deleteById(1);
+//Fernet.deleteAll();
