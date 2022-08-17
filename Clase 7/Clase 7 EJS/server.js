563b5a6ed380c534d553339198a1a51123587f97
Cristobal Rodriguez <tobal.rodriguez@gmail.com>
2022-08-17 16:54:26 -0300

    post revision con tutor

--- Clase 7/Clase 7 EJS/server.js
+++ Clase 7/Clase 7 EJS/server.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const productos = 'clase4Arr.json'
 const PORT = 8080
+const Productos = require('./Container.js')
+const contenedor = new Productos()
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
     console.log(`Server esta corriendo en el puerto ${PORT}`)
 })
 app.set('view engine', 'ejs')
-app.get('/productos', (req,res) => {
-    res.render('index', {productos})
+app.set('views','./views')
+app.get('/productos', async (req,res) => {
+    const todosProductos = await contenedor.getAll()
+    res.render('index', {productos: todosProductos})
+    console.log(todosProductos)
 })
--- Clase 7/Clase 7 Pug/server.js
+++ Clase 7/Clase 7 Pug/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
+const Productos = require('./Container.js')
+const db = './clase4Arr.json'
+const contenedor = new Productos(db)
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.set('views','./views')
+app.set('view engine', 'pug')
 
+
+app.get('/productos', async (req,res) =>  {
+    const todosProductos = await contenedor.getAll()
+    res.render('index', {productos: todosProductos})
+    console.log(todosProductos)
+})
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
-
-app.set('view engine', 'pug')
-app.set('views','./views')
-
-app.get('/productos')
